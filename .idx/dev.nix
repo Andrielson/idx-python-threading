c7d932ed2cabfbf4ede247fed714c141564f8121
andrielson <andrielson@gmail.com>
2024-06-24 14:43:57 +0000

    feat: add package htop

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  packages = [ pkgs.htop pkgs.python3 ];
   services = {
     postgres = {
       enable = true;
